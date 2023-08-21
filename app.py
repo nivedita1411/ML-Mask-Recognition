b61add64b48780af8dd7ab82ae4d7904d866c481
nivedita1411 <niveditayadav55@gmail.com>
2023-08-22 01:12:54 +0530

    ML Project

---
+++ app.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, Response
+import cv2
+import imutils
+import numpy as np
+import time
+
+app = Flask(__name__)
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    # Your mask detection code here
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),(104.0, 177.0, 123.0))
+
+	# pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+    print(detections.shape)
+
+	# initialize our list of faces, their corresponding locations,
+	# and the list of predictions from our face mask network
+    faces = []
+    locs = []
+    preds = []
+
+	# loop over the detections
+    for i in range(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the detection
+	    confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the confidence is
+		# greater than the minimum confidence
+        if confidence > 0.5:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the object
+		    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+			# ensure the bounding boxes fall within the dimensions of
+			# the frame
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+			# extract the face ROI, convert it from BGR to RGB channel
+			# ordering, resize it to 224x224, and preprocess it
+		    face = frame[startY:endY, startX:endX]
+		    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+		    face = cv2.resize(face, (224, 224))
+		    face = img_to_array(face)
+		    face = preprocess_input(face)
+
+			# add the face and bounding boxes to their respective
+			# lists
+		    faces.append(face)
+		    locs.append((startX, startY, endX, endY))
+
+	# only make a predictions if at least one face was detected
+    if len(faces) > 0:
+		# for faster inference we'll make batch predictions on *all*
+		# faces at the same time rather than one-by-one predictions
+		# in the above `for` loop
+	    faces = np.array(faces, dtype="float32")
+	    preds = maskNet.predict(faces, batch_size=32)
+
+	# return a 2-tuple of the face locations and their corresponding
+	# locations
+    return (locs, preds)
+
+# load our serialized face detector model from disk
+    
+
+def generate_frames():
+    prototxtPath = "face_detector/deploy.prototxt"
+    weightsPath = "face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+    faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+    maskNet = cv2.dnn.readNet("mask_detector.model")
+
+    vs = cv2.VideoCapture(0)
+    loop_duration = 10
+    start_time = time.time()
+
+    while True:
+        ret, frame = vs.read()
+        if not ret:
+            break
+
+        frame = imutils.resize(frame, width=400)
+        (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+        # Your mask detection drawing code here
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        elapsed_time = time.time() - start_time
+        if elapsed_time > loop_duration:
+            break
+
+    vs.release()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
